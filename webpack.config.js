dce93809ca9e4a092a7765de834eaf1bb9a16173
Edward Choi <edwardchoi.2011@gmail.com>
2019-03-06 15:07:50 -0800

    seperated CSS files from bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CSSExtract = new ExtractTextPlugin('styles.css');
 
 module.exports = (env) => {
 	const isProduction = env === 'production';
@@ -18,22 +20,28 @@ module.exports = (env) => {
 				exclude: /node_modules/
 			}, {
 				test: /\.s?css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
+				use: CSSExtract.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}					
+					]
+				})
 			}]
 		},
-	
-		resolveLoader: {
-			modules: [
-			  'node_modules'
-			]
-		},
-	
-		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-		
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',		
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
 			historyApiFallback: true	
