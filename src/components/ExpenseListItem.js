53106aad68c212c15801cfdec97a64b17912e324
Edward Choi <edwardchoi.2011@gmail.com>
2019-03-07 13:08:25 -0800

    formatted date and amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
-            <h3>Description: {description}</h3>
+            <h3>{description}</h3>
         </Link>
-        <p>Amount: {amount}</p>
-        <p>Created at: {createdAt}</p>
+        <p>
+            Amount: {numeral(amount / 100).format('$0,0.00')}
+        </p>
+        <p>
+            Created at: {moment(createdAt).format('MMM do, YYYY')}
+        </p>
     </div>    
 );
 
