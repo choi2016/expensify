e9cecd9d33642040f0d613606ee6cc712650acea
Edward Choi <edwardchoi.2011@gmail.com>
2019-03-11 17:25:00 -0700

    set up testing database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDrNMjkbJIiXqGIF0UDAT-zw4xpLJvvkzs",
-    authDomain: "wallet-e5c4b.firebaseapp.com",
-    databaseURL: "https://wallet-e5c4b.firebaseio.com",
-    projectId: "wallet-e5c4b",
-    storageBucket: "wallet-e5c4b.appspot.com",
-    messagingSenderId: "493097549151"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
